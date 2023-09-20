9fdcf1fbbfa0666b6b0b1cb7f8996ba2c151318f
aelbore <aelbore.sg@gmail.com>
2023-09-20 22:15:12 +0800

    workaround for minify

--- src/swc.ts
+++ src/swc.ts
@@ -3,21 +3,21 @@ import type { Plugin } from 'rollup'
 import { existsSync, statSync } from 'fs'
 import { dirname, extname, join } from 'path'
 
-import { Options, transformSync, minifySync, JsMinifyOptions } from '@swc/core'
+import { Options, transformSync } from '@swc/core'
 import { createDefaultConfig } from './register'
 
-const EXTENSIONS = [ 'ts', 'tsx' ] 
+const EXTENSIONS = [ 'ts', 'tsx' ]
 
 function pathResolver(extensions?: string[]) {
-  const exts = [ ...EXTENSIONS, 'js', 'jsx', ...(extensions ?? []) ] 
+  const exts = [ ...EXTENSIONS, 'js', 'jsx', ...(extensions ?? []) ]
 
   const resolveFile = (resolved: string, index: boolean = false) => {
     for (const extension of exts) {
-      const file = index 
+      const file = index
         ? join(resolved, `index.${extension}`)
         : `${resolved}.${extension}`
       if (existsSync(file)) {
-        return file 
+        return file
       }
     }
     return null
@@ -35,12 +35,28 @@ function pathResolver(extensions?: string[]) {
         if (coreFile) {
           return coreFile
         }
-      }  
+      }
     }
   }
 }
 
-export function swcPlugin(options?: Options) {  
+function minify(code: string, options?: Options) {
+  return transformSync(code, {
+    jsc: {
+      parser: { syntax: 'ecmascript' },
+      target: options?.jsc?.target ?? 'es2022',
+      minify: {
+        compress: true,
+        mangle:  { toplevel: true }
+      }
+    },
+    sourceMaps: (options && typeof options?.sourceMaps == 'string')
+      ? true: options?.sourceMaps,
+    minify: true
+  })
+}
+
+export function swcPlugin(options?: Options) {
   const extensions = EXTENSIONS.map(ext => `.${ext}`)
   const filter = (id: string) => extensions.includes(extname(id))
 
@@ -54,12 +70,7 @@ export function swcPlugin(options?: Options) {
       return filter(id) && transformSync(code, { filename: id, ...opts })
     },
     renderChunk(code: string) {
-      return options?.minify 
-        ? minifySync(code, {
-          sourceMap: (typeof opts.sourceMaps == 'string') ? true: opts.sourceMaps, 
-          ...({ compress: true, mangle: { toplevel: true } } as JsMinifyOptions),  
-        })
-        : null
+      return options?.minify ? minify(code, opts): null
     }
   } as Plugin
 }
